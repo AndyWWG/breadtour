6bb4f13caef6bdc997eb591ccf7f24a0033269dd
AndyWWG <17217andy@163.com>
2016-06-29 09:22:11 +0800

    233333333

---
+++ app/src/main/java/com/example/com/breadtour/ActivityLayout.java
@@ -0,0 +1,7 @@
+package com.example.com.breadtour;
+
+/**
+ * Created by Administrator on 2016/6/29 0029.
+ */
+public class ActivityLayout {
+}
---
+++ app/src/main/java/com/example/com/breadtour/find/entity/meassagebean.java
@@ -0,0 +1,255 @@
+package com.example.com.breadtour.find.entity;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/6/28 0028.
+ */
+public class meassagebean {
+
+    /**
+     * publish_date : 20160628183720
+     * next_start : 30
+     * product_list : [{"tab_list":["线上活动"],"distance":"","product_id":15673,"title":"招募装备体验师｜2元抽探路者60L登山徒步双肩包！","title_page":"http://photos.breadtrip.com/hunter/product/cover/590846_o_1am0ug1iq1h1v1v0315tl1soqam9.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":11,"status":"prebook","user":{"id":2384326911,"avatar_l":"http://photos.breadtrip.com/avatar_1f_d3_80550f9dc434ecd87f217de0eecf6bcca90b8923.jpg-avatar.l","name":"拾叁先生"},"address_display_type":2,"address":"远程","sold_count":99,"date_str":"07.03-09.25","price":"2","stock":2587},{"tab_list":["经验分享","目的地咨询","线上活动"],"distance":"","product_id":15494,"title":"微信群分享：4个月只花500块还能住别墅？打工换宿指南分享","title_page":"http://photos.breadtrip.com/hunter/product/cover/1630381_o_1alhb7b771s6b1f7f11ed1j2f1ht79.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":99,"status":"prebook","user":{"id":2384434553,"avatar_l":"http://photos.breadtrip.com/avatar_b4_3b_52ed1310e41ac79d7ae208e8aa83090f1f7c231b.jpg-avatar.l","name":"对对Duidui"},"address_display_type":2,"address":"远程","sold_count":91,"date_str":"06.30-08.14","price":"5","stock":208},{"tab_list":["技能get","经验分享","线上活动"],"distance":"","product_id":15553,"title":"微信群分享：分享我在澳洲打工度假和换宿的日子","title_page":"http://photos.breadtrip.com/hunter/product/cover/1573689_o_1alopvuabhnvnb6paq72bchth.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":13,"status":"prebook","user":{"id":2384429630,"avatar_l":"http://photos.breadtrip.com/avatar_ff_93_102670925ee158a18d13e341bb2973eb73bfa803.jpg-avatar.l","name":"毛駄哥哥"},"address_display_type":2,"address":"远程","sold_count":63,"date_str":"12.31-01.07","price":"2","stock":189},{"tab_list":["技能get","语言","线上活动"],"distance":"","product_id":13541,"title":"微信群分享：新闻+原著，跟着外教练纯正英伦腔","title_page":"http://photos.breadtrip.com/hunter/product/cover/967297_o_1ai0abg391sg1hpesi6t6v15tf9.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":238,"status":"prebook","user":{"id":2384237431,"avatar_l":"http://photos.breadtrip.com/avatar_26_53_a979e6290a67fbdc72a8f7906419d440af5ece7f.jpg-avatar.l","name":"Matt小满"},"address_display_type":2,"address":"远程","sold_count":395,"date_str":"06.28-07.31","price":"10","stock":3384},{"tab_list":["经验分享","语言","线上活动"],"distance":"","product_id":15269,"title":"微信群分享：每天学一句韩语，轻松勾搭欧巴欧尼","title_page":"http://photos.breadtrip.com/hunter/product/cover/175898_o_1al1im098vj392k1sg41j01a4o9.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":34,"status":"prebook","user":{"id":2384202257,"avatar_l":"http://photos.breadtrip.com/avatar_97_38_65d1cd780ac666953bafe92066eea2f519f20e2d.jpg-avatar.l","name":"马小喵_喵_喵"},"address_display_type":2,"address":"远程","sold_count":101,"date_str":"06.28-08.14","price":"2","stock":4773},{"tab_list":["美食","技能get","经验分享","线上活动"],"distance":"","product_id":15546,"title":"微信群分享：海归设计师告诉你那些美貌的蛋糕是怎么做出来的","title_page":"http://photos.breadtrip.com/hunter/product/cover/603711_o_1alp4v62rhl1a4j1q1i1pi9rtd9.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":38,"status":"prebook","user":{"id":2384206910,"avatar_l":"http://photos.breadtrip.com/avatar_d8_d9_e9ed577507248ff407d55b76f6f8deddbff492b8.jpg-avatar.l","name":"miffy_shan"},"address_display_type":2,"address":"远程","sold_count":26,"date_str":"07.03-09.25","price":"5","stock":1292},{"tab_list":["技能get","经验分享","语言","线上活动"],"distance":"","product_id":15489,"title":"微信群分享：会演讲的人成功几率比别人高2倍","title_page":"http://photos.breadtrip.com/hunter/product/cover/3030061_o_1alh7l4d21ffl36bdbtb9q180ho.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":22,"status":"prebook","user":{"id":2384434924,"avatar_l":"http://photos.breadtrip.com/avatar_d2_84_6453532db68e9688576483f77bce1424ce888657.jpg-avatar.l","name":"亦周老师"},"address_display_type":2,"address":"远程","sold_count":31,"date_str":"06.29-07.13","price":"10","stock":569},{"tab_list":["技能get","经验分享","线上活动"],"distance":"","product_id":15483,"title":"微信群分享：人脉=人卖，微信朋友圈人脉管理秘笈","title_page":"http://photos.breadtrip.com/hunter/product/cover/510650_o_1alh431tm8921m1i16kovpn94o1c.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":28,"status":"prebook","user":{"id":2384073079,"avatar_l":"http://photos.breadtrip.com/avatar_bf_4d_af0f5915540cad22149006e9020313634e14c7f7.jpg-avatar.l","name":"逛逛诳诳"},"address_display_type":2,"address":"远程","sold_count":39,"date_str":"06.30","price":"5","stock":161},{"tab_list":["经验分享","线上活动"],"distance":"","product_id":15542,"title":"微信群分享：用《使徒行者》的方式打开《余罪》","title_page":"http://photos.breadtrip.com/hunter/product/cover/62359_o_1alogjnmc1c2le54o1q791ii69.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":3,"status":"prebook","user":{"id":2384318211,"avatar_l":"http://photos.breadtrip.com/avatar_da_0d_f241253d50dfb3eb3f07c9558abc74bfc7170626.jpg-avatar.l","name":"肉肉肉肉肉包"},"address_display_type":2,"address":"远程","sold_count":24,"date_str":"07.10","price":"10","stock":30},{"tab_list":["美食","经验分享","线上活动"],"distance":"","product_id":15378,"title":"微信群分享：优雅生活从葡萄酒开始，来自法国的纯正味道","title_page":"http://photos.breadtrip.com/hunter/product/cover/701012_o_1al9css431m20k11h8l1noa15k3e.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":35,"status":"prebook","user":{"id":2384208121,"avatar_l":"http://photos.breadtrip.com/avatar_e0_88_e5daf3e2d846ca3483ea4e46be31f34c0ed03d64.jpg-avatar.l","name":"Rémy 嘿米"},"address_display_type":2,"address":"远程","sold_count":47,"date_str":"07.10-08.28","price":"5","stock":399},{"tab_list":["运动健身","经验分享","线上活动"],"distance":"","product_id":14321,"title":"微信群分享：定制你的健身计划，一周练出马甲线","title_page":"http://photos.breadtrip.com/hunter/product/cover/573841_o_1aj3ojk6m12sp1t881cq1cbi1j19.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":197,"status":"prebook","user":{"id":2384236118,"avatar_l":"http://photos.breadtrip.com/avatar_17_c9_3f45a3c532c61c02bf6238b0ccdc76f299ad5c21.jpg-avatar.l","name":"我是周哥周梦萦"},"address_display_type":2,"address":"远程","sold_count":111,"date_str":"06.30","price":"15","stock":10},{"tab_list":["经验分享","目的地咨询","线上活动"],"distance":"","product_id":13283,"title":"微信群分享：达人教你玩转日本，聊聊你不知道的事","title_page":"http://photos.breadtrip.com/hunter/product/cover/1431761_o_1ahg767ftln01dak1uft486ear9.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":130,"status":"prebook","user":{"id":2384274828,"avatar_l":"http://photos.breadtrip.com/avatar_9c_56_a91f9c622bdae00702ba1b990a6a51777d54e2f8.jpg-avatar.l","name":"早稻田的易林桑"},"address_display_type":2,"address":"远程","sold_count":105,"date_str":"06.28-07.12","price":"5","stock":168},{"tab_list":["个性定制","线上活动"],"distance":"","product_id":13071,"title":"线上定制：字如其人，让我为你设计一个高颜值签名","title_page":"http://photos.breadtrip.com/hunter/product/cover/68207_o_1aht1phn0169nuntp8f1mki1bdr9.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":153,"status":"prebook","user":{"id":2384236578,"avatar_l":"http://photos.breadtrip.com/avatar_07_13_1f30a4e61a351a53a23d9715dfb619ba03911a10.jpg-avatar.l","name":"墨点签名设计"},"address_display_type":2,"address":"远程","sold_count":115,"date_str":"06.28-07.31","price":"20","stock":677},{"tab_list":["聚会","线上活动"],"distance":"","product_id":14441,"title":"来打卡啦！把你那些压箱底的书拿出来读完吧！","title_page":"http://photos.breadtrip.com/hunter/product/cover/229235_o_1aj93doe11abrc5p18gs1g8q19dif.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":14,"status":"prebook","user":{"id":2384301687,"avatar_l":"http://photos.breadtrip.com/avatar_e0_f1_30fa9f42aa0ed7558e78e5a6e12041dbec224fb5.jpg-avatar.l","name":"正义地球人喏"},"address_display_type":2,"address":"远程","sold_count":42,"date_str":"07.04-12.26","price":"10","stock":123},{"tab_list":["经验分享","线上活动"],"distance":"","product_id":14882,"title":"微信群分享：穷游欧洲20国 机智的我只用了3000块","title_page":"http://photos.breadtrip.com/hunter/product/cover/46318_o_1ak2n45glus61nommoh1h4f61v1b.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":98,"status":"prebook","user":{"id":2384274290,"avatar_l":"http://photos.breadtrip.com/avatar_84_df_601c6f3889f46d8eb65810557fad652dbfe939fb.jpg-avatar.l","name":"倪大宝"},"address_display_type":2,"address":"远程","sold_count":101,"date_str":"06.28-09.24","price":"10","stock":6896},{"tab_list":["经验分享","线上活动"],"distance":"","product_id":15547,"title":"微信群分享：终南山，27岁心灵的宁静回归","title_page":"http://photos.breadtrip.com/hunter/product/cover/301913_o_1alpgaggv1smsts3bdpkdh1os3c.png?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":11,"status":"prebook","user":{"id":2384219888,"avatar_l":"http://photos.breadtrip.com/avatar_0e_dc_9046ae38d75a46222fa0aa57cf83ee345e3d6c7d.jpg-avatar.l","name":"小强环球"},"address_display_type":2,"address":"远程","sold_count":17,"date_str":"07.03-09.25","price":"2","stock":1299},{"tab_list":["摄影","技能get","经验分享","线上活动"],"distance":"","product_id":14667,"title":"微信群分享：如何正确提升朋友圈照片的逼格","title_page":"http://photos.breadtrip.com/hunter/product/cover/217292_o_1ajluscqe14m61geh15he15s3n099.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":117,"status":"prebook","user":{"id":2384203667,"avatar_l":"http://photos.breadtrip.com/avatar_94_24_301cdef9d2acc33d5ba64b1af4baf2ca706b4c00.jpg-avatar.l","name":"六年Summer"},"address_display_type":2,"address":"远程","sold_count":298,"date_str":"07.02-08.27","price":"10","stock":979},{"tab_list":["绘画","线上活动"],"distance":"","product_id":14219,"title":"萌翻宇宙！女神经老师带你从零基础变身插画小达人","title_page":"http://photos.breadtrip.com/hunter/product/cover/422543_o_1aiufq9dgn3ia924gb1jga13tf9.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":221,"status":"prebook","user":{"id":2384098106,"avatar_l":"http://photos.breadtrip.com/avatar_77_c8_72d3c163c0e951eb284b2c9cb55eebfb4d2304c9.jpg-avatar.l","name":"Lulla_Queen"},"address_display_type":2,"address":"远程","sold_count":188,"date_str":"07.04-10.31","price":"40","stock":768},{"tab_list":["美食","技能get","经验分享","线上活动"],"distance":"","product_id":13330,"title":"还在做烹饪小白？300本电子厨艺书籍等你来拿！","title_page":"http://photos.breadtrip.com/hunter/product/cover/146489_o_1ahgd9brb16od787lu3dbdd70t.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":151,"status":"prebook","user":{"id":2384274393,"avatar_l":"http://photos.breadtrip.com/avatar_ec_e5_c78dff4a4ba2f372307fa3d9e0490936b1287d34.jpg-avatar.l","name":"Junefwo"},"address_display_type":2,"address":"远程","sold_count":98,"date_str":"06.29-07.31","price":"6","stock":3295},{"tab_list":["绘画","线上活动"],"distance":"","product_id":12510,"title":"七天手绘日记养成记，收获一本七彩的生活写真","title_page":"http://photos.breadtrip.com/hunter/product/cover/717419_o_1aeu0cbho1lhl1663hva1e4nelfh.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/","is_new":"","discount_price":"","like_count":413,"status":"prebook","user":{"id":2384098106,"avatar_l":"http://photos.breadtrip.com/avatar_77_c8_72d3c163c0e951eb284b2c9cb55eebfb4d2304c9.jpg-avatar.l","name":"Lulla_Queen"},"address_display_type":2,"address":"远程","sold_count":347,"date_str":"07.08-07.22","price":"40","stock":113}]
+     * became_hunter_url : http://web.breadtrip.com/m/club/join_city_hunter/
+     */
+
+    private String publish_date;
+    private int next_start;
+    private String became_hunter_url;
+    /**
+     * tab_list : ["线上活动"]
+     * distance :
+     * product_id : 15673
+     * title : 招募装备体验师｜2元抽探路者60L登山徒步双肩包！
+     * title_page : http://photos.breadtrip.com/hunter/product/cover/590846_o_1am0ug1iq1h1v1v0315tl1soqam9.jpg?imageView2/5/w/788/h/426/format/jpg/interlace/1/
+     * is_new :
+     * discount_price :
+     * like_count : 11
+     * status : prebook
+     * user : {"id":2384326911,"avatar_l":"http://photos.breadtrip.com/avatar_1f_d3_80550f9dc434ecd87f217de0eecf6bcca90b8923.jpg-avatar.l","name":"拾叁先生"}
+     * address_display_type : 2
+     * address : 远程
+     * sold_count : 99
+     * date_str : 07.03-09.25
+     * price : 2
+     * stock : 2587
+     */
+
+    private List<ProductListBean> product_list;
+
+    public String getPublish_date() {
+        return publish_date;
+    }
+
+    public void setPublish_date(String publish_date) {
+        this.publish_date = publish_date;
+    }
+
+    public int getNext_start() {
+        return next_start;
+    }
+
+    public void setNext_start(int next_start) {
+        this.next_start = next_start;
+    }
+
+    public String getBecame_hunter_url() {
+        return became_hunter_url;
+    }
+
+    public void setBecame_hunter_url(String became_hunter_url) {
+        this.became_hunter_url = became_hunter_url;
+    }
+
+    public List<ProductListBean> getProduct_list() {
+        return product_list;
+    }
+
+    public void setProduct_list(List<ProductListBean> product_list) {
+        this.product_list = product_list;
+    }
+
+    public static class ProductListBean {
+        private String distance;
+        private int product_id;
+        private String title;
+        private String title_page;
+        private String is_new;
+        private String discount_price;
+        private int like_count;
+        private String status;
+        /**
+         * id : 2384326911
+         * avatar_l : http://photos.breadtrip.com/avatar_1f_d3_80550f9dc434ecd87f217de0eecf6bcca90b8923.jpg-avatar.l
+         * name : 拾叁先生
+         */
+
+        private UserBean user;
+        private int address_display_type;
+        private String address;
+        private int sold_count;
+        private String date_str;
+        private String price;
+        private int stock;
+        private List<String> tab_list;
+
+        public String getDistance() {
+            return distance;
+        }
+
+        public void setDistance(String distance) {
+            this.distance = distance;
+        }
+
+        public int getProduct_id() {
+            return product_id;
+        }
+
+        public void setProduct_id(int product_id) {
+            this.product_id = product_id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getTitle_page() {
+            return title_page;
+        }
+
+        public void setTitle_page(String title_page) {
+            this.title_page = title_page;
+        }
+
+        public String getIs_new() {
+            return is_new;
+        }
+
+        public void setIs_new(String is_new) {
+            this.is_new = is_new;
+        }
+
+        public String getDiscount_price() {
+            return discount_price;
+        }
+
+        public void setDiscount_price(String discount_price) {
+            this.discount_price = discount_price;
+        }
+
+        public int getLike_count() {
+            return like_count;
+        }
+
+        public void setLike_count(int like_count) {
+            this.like_count = like_count;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public UserBean getUser() {
+            return user;
+        }
+
+        public void setUser(UserBean user) {
+            this.user = user;
+        }
+
+        public int getAddress_display_type() {
+            return address_display_type;
+        }
+
+        public void setAddress_display_type(int address_display_type) {
+            this.address_display_type = address_display_type;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public int getSold_count() {
+            return sold_count;
+        }
+
+        public void setSold_count(int sold_count) {
+            this.sold_count = sold_count;
+        }
+
+        public String getDate_str() {
+            return date_str;
+        }
+
+        public void setDate_str(String date_str) {
+            this.date_str = date_str;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
+
+        public int getStock() {
+            return stock;
+        }
+
+        public void setStock(int stock) {
+            this.stock = stock;
+        }
+
+        public List<String> getTab_list() {
+            return tab_list;
+        }
+
+        public void setTab_list(List<String> tab_list) {
+            this.tab_list = tab_list;
+        }
+
+        public static class UserBean {
+            private long id;
+            private String avatar_l;
+            private String name;
+
+            public long getId() {
+                return id;
+            }
+
+            public void setId(long id) {
+                this.id = id;
+            }
+
+            public String getAvatar_l() {
+                return avatar_l;
+            }
+
+            public void setAvatar_l(String avatar_l) {
+                this.avatar_l = avatar_l;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
+}
