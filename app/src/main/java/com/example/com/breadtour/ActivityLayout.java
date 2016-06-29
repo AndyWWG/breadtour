56a713e80ca33816f66c1702db4e524d14eb53fb
kaiyea <757501392@qq.com>
2016-06-29 20:28:11 +0800

    777

--- app/src/main/java/com/example/com/breadtour/ActivityLayout.java
+++ app/src/main/java/com/example/com/breadtour/ActivityLayout.java
@@ -1,7 +1,28 @@
 package com.example.com.breadtour;
 
+import com.example.com.breadtour.utils.ui.BaseActivity;
+
 /**
  * Created by Administrator on 2016/6/29 0029.
  */
-public class ActivityLayout {
+public class ActivityLayout extends BaseActivity{
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initEvents() {
+
+    }
 }
